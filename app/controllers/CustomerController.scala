c3bc64f64d10488d5707f797d1f0be28ffc4eacc
ZeroPath <noreply@zeropath.com>
2025-07-16 05:20:33 +0000

    refactor: improve customer ID validation and SQL parameterization

--- app/controllers/CustomerController.scala
+++ app/controllers/CustomerController.scala
@@ -105,7 +105,9 @@ class CustomerController @Inject() (ws: WSClient, config: Configuration) extends
   // get /rawcustomers/{customerId}
   def getRawCustomer(customerId: String) = Action {
     if (null == customerId) throw new InvalidCustomerRequestException
+    // Validate and convert customerId to Long; throws InvalidCustomerRequestException on invalid format
     val id = try { customerId.toLong } catch { case _: NumberFormatException => throw new InvalidCustomerRequestException }
+    // Use parameterized SQL to prevent SQL injection
     val rawSql = RawSqlBuilder.parse("SELECT first_name, last_name FROM customer WHERE id = :id").create
     val query = CustomerController.db.find(classOf[Customer])
     query.setRawSql(rawSql)
